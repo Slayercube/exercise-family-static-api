b5be12cddb6c1ead4cd889d95251bf7122475a4e
Deepak <160626769+Slayercube@users.noreply.github.com>
2024-07-27 17:58:56 +0000

    sidhu_commit

--- src/app.py
+++ src/app.py
@@ -1,85 +1,74 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
-from datastructures import FamilyMember
-#from models import Person
+from datastructures import FamilyStructure, FamilyMember
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
+
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
-me = FamilyMember("sidhu", 30, [8, 2])
-print(me)
+user = FamilyMember("deep", 30, [1, 5])
 
-sidhu_family = FamilyStructure("sidhu")
-sidhu_family.add_member(me)
-print(sidhu_family.last_name)
-for member in sidhu_family._members:
-    print(member.first_name)
+sidhu_family = FamilyStructure('sidhu')
+sidhu_family.add_member(user)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-    return jsonify(response_body), 200
+def get_members():
+    try:
+        members = sidhu_family.get_all_members()
+        response_body = [member.__dict__ for member in members]
+        return jsonify(response_body), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/member/<int:id>', methods=['GET'])
-def get_member(id):
-    member = sidhu_family.get_member(id)
-    response_body = {
-        "hello": "world",
-        "family": member
-    }
-    return jsonify(response_body), 200
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    try:
+        member = sidhu_family.get_member(member_id)
+        if member:
+            return jsonify(member.__dict__), 200
+        else:
+            return jsonify({"error": "Member not found"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-@app.route('/member/<int:id>', methods=['POST'])
-def add_member(id):
-    member = sidhu_family.add_member(id)
-    response_body = {
-        "hello": "world",
-        "family": member
-    }
-    return jsonify(response_body), 200
+@app.route('/member', methods=['POST'])
+def add_member():
+    try:
+        data = request.get_json()
+        if not data or not all(k in data for k in ("first_name", "age", "lucky_numbers")):
+            return jsonify({"error": "Invalid input"}), 400
+        new_member = FamilyMember(data["first_name"], data["age"], data["lucky_numbers"])
+        sidhu_family.add_member(new_member)
+        return jsonify(new_member.__dict__), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    try:
+        result = sidhu_family.delete_member(member_id)
+        if result:
+            return jsonify({"done": True}), 200
+        else:
+            return jsonify({"error": "Member not found"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
-
-@app.route('/member/<int:id>', methods=['DELETE'])
-def delete_member(id):
-    member = sidhu_family.delete_member(id)
-    response_body = {
-        "hello": "world",
-        "family": member
-    }
-    return jsonify(response_body), 200
-
-
-
+# this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
-
-
-
-
-
-
-
-
-
-
--- src/datastructures.py
+++ src/datastructures.py
@@ -1,63 +1,37 @@
-"""
-update this file to implement the following already declared methods:
-- add_member: Should add a member to the self._members list
-- delete_member: Should delete a member from the self._members list
-- update_member: Should update a member from the self._members list
-- get_member: Should return a member from the self._members list
-"""
-from random import randint
+import random
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-        # example list of members
         self._members = []
-        self.next_id = 1
-    # read-only: Use this method to generate random members ID's when adding members into the list
+
     def _generateId(self):
-        return randint(0, 99999999)
+        return random.randint(0, 99999999)
+
     def add_member(self, member):
-        # fill this method and update the return
-        member.lastName = self.last_name
-        member.id = self.next_id +1
+        member.id = self._generateId()
+        member.last_name = self.last_name
         self._members.append(member)
-        
+
     def delete_member(self, id):
-        # fill this method and update the return
-        self._members = [x for x in self._members if x.id != id]
-        return self._members
-      
-        
+        self._members = [member for member in self._members if member.id != id]
+
     def get_member(self, id):
         for member in self._members:
-            if member['id']== id:
+            if member.id == id:
                 return member
-        # fill this method and update the return
-        
-        
+        return None
 
-        
-    # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
-    
-    
+
 class FamilyMember:
-    last_name = None
     def __init__(self, first_name, age, lucky_numbers):
+        self.id = None
         self.first_name = first_name
         self.age = age
         self.lucky_numbers = lucky_numbers
+        self.last_name = None
+
     def __str__(self):
-        return f"{self.first_name} {self.age} {self.last_name}"
-
-
-
-
-
-
-
-
-
-
-
-
+        return f"{self.first_name} {self.last_name}, Age: {self.age}, Lucky Numbers: {self.lucky_numbers}"
